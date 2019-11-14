de6afd200cde01f373499b167125669f71513340
Ivan <kombat_kos@mail.ru>
2019-11-14 10:47:03 +0400

    Initial Commit

---
+++ passData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passData
+//
+//  Created by kot on 14/11/2019.
+//  Copyright © 2019 Konstantin Porokhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
